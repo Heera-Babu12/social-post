a1388c7ad3ce08873a354337b3fc745621fe18af
Heera K B <157723095+Heera-Babu12@users.noreply.github.com>
2025-11-06 12:13:00 +0530

    Add files via upload

---
+++ modules/navigation.php
@@ -0,0 +1,8 @@
+
+<div class="navbar">
+    <span class="leftbarnav"><a href="home.php">Picky Assist - Home </a></span>
+    <div class="nav-links">
+        <a href="myprofile.php">My Profile</a>
+        <a href="index.php?doAction=logout">Logout</a>
+    </div>
+</div>
