a783dff2cab2cba56c24f582d3a42fd1470d1428
Heera K B <157723095+Heera-Babu12@users.noreply.github.com>
2025-11-06 12:11:19 +0530

    Create config.php

---
+++ config/config.php
@@ -0,0 +1,14 @@
+<?php
+
+// File for Credentials storing
+
+// DB CREDENTIALS
+$db_host = "localhost";
+$db_username = "root";
+$db_password = "";
+$db_database = "picky_assist_test";
+
+// ENCRYPTION KEY
+define("ENCKEY", "HAJ8956HK752L63RF9H3");
+
+?>
